2896702c38981dfd8d6e15d70563b661eb7b9af0
Tosin Shada <tosinshada@gmail.com>
2023-09-18 01:40:40 +0100

    Update frontend

--- frontend/hooks/useDomain.ts
+++ frontend/hooks/useDomain.ts
@@ -1,5 +1,6 @@
-import { available, register } from '@tide/sns-registrar'
-import { name, setName } from '@tide/sns-resolver'
+import { Contract as registrarContract, networks as registrarNetwork } from 'sns-registrar-client'
+import { Contract as resolverContract, networks as resolverNetwork, Address } from 'sns-resolver-client'
+import freighter from "@stellar/freighter-api";
 import { useAccount } from './useAccount'
 import { useState } from 'react'
 import { createHash } from 'crypto'
@@ -8,12 +9,33 @@ import { useToast } from '@/components/ui/use-toast'
 export function useDomain() {
   const [domainAvailable, setDomainAvailable] = useState<boolean>(false)
   const [domainAddress, setDomainAddress] = useState<string>('')
-  const [checked, setChecked] = useState<boolean>(false)
+  const [checked, setChecked] = useState<boolean>(false) 
   const account = useAccount()
   const { toast } = useToast()
 
+  const { name } = new resolverContract({
+    contractId: resolverNetwork.futurenet.contractId,
+    networkPassphrase: resolverNetwork.futurenet.networkPassphrase,
+    rpcUrl: 'https://rpc-futurenet.stellar.org:443',
+    wallet: freighter,
+  })
+
+  // const { available, register } = new registrarContract({
+  //   contractId: registrarNetwork.futurenet.contractId,
+  //   networkPassphrase: registrarNetwork.futurenet.networkPassphrase,
+  //   rpcUrl: 'https://rpc-futurenet.stellar.org:443',
+  //   wallet: freighter,
+  // })
+
   async function registerDomain(username: string, address: string) {
     try {
+      const { available, register } = new registrarContract({
+        contractId: registrarNetwork.futurenet.contractId,
+        networkPassphrase: registrarNetwork.futurenet.networkPassphrase,
+        rpcUrl: 'https://rpc-futurenet.stellar.org:443',
+        wallet: freighter,
+      })
+      
       const domain = createHash('sha256')
         .update(username.toLowerCase())
         .digest()
@@ -25,18 +47,13 @@ export function useDomain() {
       }
 
       await register({
-        caller: account.address,
-        owner: address,
+        caller: Address.fromString(account.address),
+        owner: Address.fromString(address),
         name: domain,
+        address_name: Address.fromString(address),
         duration: 31536000n,
-      }).catch((error) => {
+      }).catch((error: any) => {
         console.error('register error', error)
-      })
-
-      await setName({
-        caller: account.address,
-        node: domain,
-        name: address,
       })
     } catch (error) {
       console.error(error)
@@ -45,7 +62,14 @@ export function useDomain() {
 
   async function checkDomainStatus(username: string): Promise<void> {
     try {
+      const { available, register } = new registrarContract({
+        contractId: registrarNetwork.futurenet.contractId,
+        networkPassphrase: registrarNetwork.futurenet.networkPassphrase,
+        rpcUrl: 'https://rpc-futurenet.stellar.org:443',
+        wallet: freighter,
+      })
       setChecked(false)
+      console.log('Checking domain status: ', username)
       if (username == null || username.length === 0) {
         return
       }
@@ -85,7 +109,7 @@ export function useDomain() {
       })
       console.log('Domain address: ', domainAddress)
 
-      setDomainAddress(domainAddress)
+      setDomainAddress(domainAddress.toString())
     } catch (error) {
       console.error(error)
       toast({
